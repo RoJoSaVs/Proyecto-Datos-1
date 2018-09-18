2f5f26bb9c0a61a052b142045fec26a6c526312d
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2018-09-18 06:31:33 -0600

    Server

---
+++ conector.java
@@ -0,0 +1,31 @@
+package sockets;
+import java.net.*;
+import java.io.*;
+
+
+public class conector {
+	ServerSocket server;
+	Socket socket;
+	int puerto = 5050;
+	DataOutputStream salida;
+	BufferedReader entrada;
+		
+	/**Inicia el servidor imprime un mensaje y cierra la conexion*/
+	public void iniciar() {
+		try {
+			server = new ServerSocket(puerto);
+			socket = new Socket();
+			socket = server.accept();
+			
+			entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));	
+			String mensaje = entrada.readLine();
+			System.out.println(mensaje);
+			salida = new DataOutputStream(socket.getOutputStream());
+			salida.writeUTF("Adios Mundo");
+			socket.close();
+		
+		}catch(Exception e) {};
+	}
+}
+
+
