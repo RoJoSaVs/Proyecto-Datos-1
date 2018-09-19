30aa46bfc941d95995f71de24a8519bd593a8c5c
AndreyZartty <43261009+AndreyZartty@users.noreply.github.com>
2018-09-19 16:11:17 -0600

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,112 @@
+package dots;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Andrey
+ */
+public class LinkedList{
+    private Node head;
+    private int size;
+    
+    public LinkedList(){
+        this.head= null;
+        this.size=0;
+    }
+    public boolean isEmpty(){
+        return this.head==null;
+    }
+    public int size(){
+        return this.size;
+    }
+    public void insertFirst(Object data){
+        Node newNode= new Node();
+        newNode.setData(data);
+        newNode.setNext(this.head);
+        this.head=newNode;
+        this.size++;
+    }
+    public Node deleteFirst(){
+        if(this.head != null){
+            Node temp= this.head;
+            this.head= this.head.getNext();
+            this.size--;
+        }
+        return null;
+    }
+    public void displayList(){
+        Node current= this.head;
+        while(current != null){
+            System.out.println(current.getData());
+            current=current.getNext();
+        }
+    }
+    public Node find(Object searchValue){
+        Node current= this.head;
+        while(current != null){
+            if (current.getData().equals(searchValue)){
+            return current;
+            }
+            else{
+                current=null;
+            }
+        }
+        return null;
+    }
+    public Node delete(Object searchValue){
+        Node current= this.head;
+        Node previous= this.head;
+        while (current != null){
+            if (current.getData().equals(searchValue)){
+                if (current.equals(this.head)){
+                    this.head = head.getNext();
+                }
+                else{
+                    previous.setNext(current.getNext());
+                }
+            return current;
+            }
+            else{
+                previous= current;
+                current=current.getNext();
+            }
+        }
+        return null;
+    }
+    public void insertLast(Object data){
+        if (this.head==null) {
+            Node nuevo = new Node();
+            nuevo.setData(data);
+            this.head = nuevo;
+            
+        } 
+        else {
+            Node head = this.head;
+            Node current_last = head;
+            while (head!=null){
+		current_last = head;
+		head = head.getNext();
+            }
+            Node nuevo = new Node();
+            nuevo.setData(data);
+            current_last.setNext(nuevo);
+        }
+        this.size++;
+    }
+    public Object recorrer(int i){
+        Node current = this.head;
+        while(i > 0){
+            current = current.getNext();
+            i -= 1;
+        }
+        return current.getData();
+    }
+}
+    
+            
+        
+            
