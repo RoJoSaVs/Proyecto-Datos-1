192e753bc2c1433501209bedfe67054e5c373494
AndreyZartty <andreysansab@gmail.com>
2018-09-21 10:24:22 -0600

    Metodo recursivo recorrerFig y modificaciones paralelas

--- Figura.java
+++ Figura.java
@@ -13,9 +13,41 @@ public class Figura {
     public int getPuntos(Figura creada){
         int puntos= creada.getLineas()*2;
         return puntos;
+    }
+    public void recorrerFig(Punto inicial, Punto recorre, Punto eliminar, LinkedList vecinos, LinkedList newFigura, int sizeEcurrent, LinkedList recorridos){
+        vecinos.delete(eliminar);
+        if (inicial.getX()==recorre.getX() && inicial.getY()==inicial.getY()){
+            recorridos.insertFirstP(inicial.getX(), inicial.getY(), inicial.getData());
+            newFigura.insertFirstP(inicial.getX(),inicial.getY(),inicial.getData());
+            fCreadas.insertFirstN(newFigura);
+            while(recorridos.size()>0){
+                recorridos.recorrerL(recorridos.size()).setAux(recorridos.recorrerL(recorridos.size()).getVecinos());
+                recorridos.delete(recorridos.recorrerL(recorridos.size()));
+            }
+        }
+        else if(sizeEcurrent>0){
+            if(recorre.Enlazado(vecinos.recorrerL(sizeEcurrent))){
+                newFigura.insertFirstP(vecinos.recorrerL(sizeEcurrent).getX(),vecinos.recorrerL(sizeEcurrent).getY(),vecinos.recorrerL(sizeEcurrent).getData());
+                sizeEcurrent--;
+                recorrerFig(inicial, vecinos.recorrerL(sizeEcurrent),recorre,vecinos.recorrerL(sizeEcurrent).getAux(),newFigura,vecinos.recorrerL(sizeEcurrent).getVecinos().size(), recorridos);
+            }
+            else{
+                recorrerFig(inicial,recorre,eliminar,vecinos,newFigura,sizeEcurrent--,recorridos);
+            }
 
+        }
+        else if(eliminar.getAux().size()>0){
+            newFigura.delete(recorre);
+            recorrerFig(inicial,eliminar,recorre,eliminar.getAux(),newFigura,eliminar.getAux().size(), recorridos);
 
+        }
+        else{
+            while(recorridos.size()>0){
+                recorridos.recorrerL(recorridos.size()).setAux(recorridos.recorrerL(recorridos.size()).getVecinos());
+                recorridos.delete(recorridos.recorrerL(recorridos.size()));
+
+            }
+        }
 
     }
-
 }
