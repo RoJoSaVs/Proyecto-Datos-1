7d5c6c71511627485a8e156d5370ad78d71c778b
AndreyZartty <andreysansab@gmail.com>
2018-09-26 13:48:30 -0600

    Reestablecimiento correcciones paralelas y creacion del atributo Estado

--- Punto.java
+++ Punto.java
@@ -3,37 +3,63 @@ package dots;
 public class Punto extends Node {
     private int x;
     private int y;
-    private LinkedList rect;
     private LinkedList vecinos;
-
-
+    private LinkedList enlazados;
+    private LinkedList enlazadosAux=enlazados;
+    private boolean estado=false;
+    
+    public void setEstado(){
+        if (this.estado==false){
+            this.estado=true;
+        }
+        else{
+            this.estado=false;
+        }
+    }
+    
+    public boolean getEstado(){
+        return this.estado;
+    }
+    
     public void setXY(int x, int y){
         this.x=x;
-        this.y=y;
+        this.y=y;    
     }
-
+    
     public int getX(){
         return this.x;
     }
     public int getY(){
         return this.y;
     }
-    public void setRect(int rectX, int rectY, int grosX, int grosY){
-        rect.insertFirst(grosY);
-        rect.insertFirst(grosX);
-        rect.insertFirst(rectY);
-        rect.insertFirst(rectX);
-    }
-    public LinkedList getRect(){
-      return this.rect;
-    }
     public LinkedList getVecinos(){
         return this.vecinos;
     }
-    public void agregarVecino(Node vecino){
-        vecinos.insertFirst(vecino.getData());
+    public LinkedList getEnlazados(){
+        return this.enlazados;
     }
-    public void borrarVecino(Node vecino){
-        vecinos.delete(vecino.getData());
+    public LinkedList getAux(){
+        return this.enlazadosAux;
+    }
+    public void setAux(LinkedList newAux){
+        this.enlazadosAux= newAux;
+    }
+    public void agregarVecino(Punto vecino){
+        vecinos.insertFirstP(vecino.getX(),vecino.getY(),vecino.getData());
+    }
+    public void agregarEnlazado(Punto vecino){
+        enlazados.insertFirstP(vecino.getX(),vecino.getY(),vecino.getData());
+    }
+    public boolean Enlazado(Punto verif){
+        int recorrido= this.enlazados.size();
+        while (recorrido>0){
+            if(verif.getX() == this.enlazados.recorrerL(recorrido).getX() && verif.getY() == this.enlazados.recorrerL(recorrido).getY() ){
+                return true;
+            }
+            else{
+                recorrido--;
+            }
+        }
+        return false;
     }
 }
