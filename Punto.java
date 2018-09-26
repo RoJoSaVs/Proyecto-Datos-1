a53d8de4cfcaba7d52815ca80dc968c39e5b24e7
Jose Solano <joseandressolanomora@gmail.com>
2018-09-26 14:56:25 -0500

    Corrección en la signación de métodos y atributos.

--- Punto.java
+++ Punto.java
@@ -3,9 +3,24 @@ package dots;
 public class Punto extends Node {
     private int x;
     private int y;
-    private LinkedList rect;
+    private LinkedList rect = new LinkedList();
     private LinkedList vecinos;
+    private LinkedList enlazados;
+    private LinkedList enlazadosAux=enlazados;
+    private boolean estado=false;
 
+    public void setEstado(){
+        if (this.estado==false){
+            this.estado=true;
+        }
+        else{
+            this.estado=false;
+        }
+    }
+
+    public boolean getEstado(){
+        return this.estado;
+    }
 
     public void setXY(int x, int y){
         this.x=x;
@@ -19,10 +34,10 @@ public class Punto extends Node {
         return this.y;
     }
     public void setRect(int rectX, int rectY, int grosX, int grosY){
-        rect.insertFirst(grosY);
-        rect.insertFirst(grosX);
-        rect.insertFirst(rectY);
-        rect.insertFirst(rectX);
+        rect.insertFirstN(grosY);
+        rect.insertFirstN(grosX);
+        rect.insertFirstN(rectY);
+        rect.insertFirstN(rectX);
     }
     public LinkedList getRect(){
       return this.rect;
@@ -31,9 +46,37 @@ public class Punto extends Node {
         return this.vecinos;
     }
     public void agregarVecino(Node vecino){
-        vecinos.insertFirst(vecino.getData());
+        vecinos.insertFirstN(vecino.getData());
     }
-    public void borrarVecino(Node vecino){
-        vecinos.delete(vecino.getData());
+
+    public LinkedList getVecinos(){
+        return this.vecinos;
+    }
+    public LinkedList getEnlazados(){
+        return this.enlazados;
+    }
+    public LinkedList getAux(){
+        return this.enlazadosAux;
+    }
+    public void setAux(LinkedList newAux){
+        this.enlazadosAux= newAux;
+    }
+    public void agregarVecino(Punto vecino){
+        vecinos.insertFirstP(vecino.getX(),vecino.getY(),vecino.getData());
+    }
+    public void agregarEnlazado(Punto vecino){
+        enlazados.insertFirstP(vecino.getX(),vecino.getY(),vecino.getData());
+    }
+    public boolean Enlazado(Punto verif){
+        int recorrido= this.enlazados.size();
+        while (recorrido>0){
+            if(verif.getX() == this.enlazados.recorrerL(recorrido).getX() && verif.getY() == this.enlazados.recorrerL(recorrido).getY() ){
+                return true;
+            }
+            else{
+                recorrido--;
+            }
+        }
+        return false;
     }
 }
