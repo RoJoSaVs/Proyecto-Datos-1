6df0e3728669ab63e63db328dd473eb8305a5113
AndreyZartty <andreysansab@gmail.com>
2018-09-28 11:21:52 -0600

    Set turno para los jugadores

--- Jugador.java
+++ Jugador.java
@@ -2,6 +2,13 @@ package dots;
 
 public class Jugador {
     private LinkedList figuras;
+    private int puntaje;
+    private int turno;
+    
+    public Jugador(int turno){
+        this.turno=turno;
+    }
+    
 
     public void setFigura(Figura nueva){
         figuras.insertFirstN(nueva);
