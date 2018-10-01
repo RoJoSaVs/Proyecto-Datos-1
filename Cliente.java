d6e7550952006ed30e9e47fed4ed8559594549bb
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2018-09-30 23:31:49 -0600

    Cliente simple integrado con interfaz

--- Cliente.java
+++ Cliente.java
@@ -1,110 +1,32 @@
 package dots;
-
-import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import java.net.*;
 
-public class Cliente {
-    private Socket socket;
-    private DataInputStream bufferDeEntrada = null;
-    private DataOutputStream bufferDeSalida = null;
-    Scanner teclado = new Scanner(System.in);
-    final String COMANDO_TERMINACION = "salir()";
+class Cliente{
 
-    public void levantarConexion(String ip, int puerto) {
-        try {
-            socket = new Socket(ip, puerto);
-            mostrarTexto("Conectado a :" + socket.getInetAddress().getHostName());
-        } catch (Exception e) {
-            mostrarTexto("Excepci�n al levantar conexi�n: " + e.getMessage());
-            System.exit(0);
+    public static void main(String[] args){
+        //public void Conexion() { 
+        
+        //Cliente client = new Cliente();
+        //client.Conexion();
+        
+        String ip = "192.168.137.1";
+        Socket conexion = null;
+        int PUERTO = 5555;
+
+        try{
+            conexion = new Socket(ip,PUERTO);
+
+            DataOutputStream flujoDatosSalida = new DataOutputStream(conexion.getOutputStream());//Creamos objeto para enviar
+            flujoDatosSalida.writeUTF("Gracias por aceptarme");  //Mandamos el mensaje al servidor
+
+        }catch(Exception e){
+
+            System.out.println("No se puedo crear la conexion");
+            e.printStackTrace();
         }
+        FrameJuego game = new FrameJuego();
+        game.Start();
     }
-
-    public static void mostrarTexto(String s) {
-        System.out.println(s);
-    }
-
-    public void abrirFlujos() {
-        try {
-            bufferDeEntrada = new DataInputStream(socket.getInputStream());
-            bufferDeSalida = new DataOutputStream(socket.getOutputStream());
-            bufferDeSalida.flush();
-        } catch (IOException e) {
-            mostrarTexto("Error en la apertura de flujos");
-        }
-    }
-
-    public void enviar(String s) {
-        try {
-            bufferDeSalida.writeUTF(s);
-            bufferDeSalida.flush();
-        } catch (IOException e) {
-            mostrarTexto("IOException on enviar");
-        }
-    }
-
-    public void cerrarConexion() {
-        try {
-            bufferDeEntrada.close();
-            bufferDeSalida.close();
-            socket.close();
-            mostrarTexto("Conexi�n terminada");
-        } catch (IOException e) {
-            mostrarTexto("IOException on cerrarConexion()");
-        }finally{
-            System.exit(0);
-        }
-    }
-
-    public void ejecutarConexion(String ip, int puerto) {
-        Thread hilo = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    levantarConexion(ip, puerto);
-                    abrirFlujos();
-                    recibirDatos();
-                } finally {
-                    cerrarConexion();
-                }
-            }
-        });
-        hilo.start();
-    }
-
-    public void recibirDatos() {
-        String st = "";
-        try {
-            do {
-                st = (String) bufferDeEntrada.readUTF();
-                mostrarTexto("\n[Servidor] => " + st);
-                System.out.print("\n[Usted] => ");
-            } while (!st.equals(COMANDO_TERMINACION));
-        } catch (IOException e) {}
-    }
-
-    public void escribirDatos() {
-        String entrada = "";
-        while (true) {
-            System.out.print("[Usted] => ");
-            entrada = teclado.nextLine();
-            if(entrada.length() > 0)
-                enviar(entrada);
-        }
-    }
-
-    public static void main(String[] argumentos) {
-        Cliente cliente = new Cliente();
-        Scanner escaner = new Scanner(System.in);
-        mostrarTexto("Ingresa la IP: [localhost por defecto] ");
-        String ip = escaner.nextLine();
-        if (ip.length() <= 0) ip = "localhost";
-
-        mostrarTexto("Puerto: [5050 por defecto] ");
-        String puerto = escaner.nextLine();
-        if (puerto.length() <= 0) puerto = "5050";
-        cliente.ejecutarConexion(ip, Integer.parseInt(puerto));
-        cliente.escribirDatos();
-    }
+    
 }
