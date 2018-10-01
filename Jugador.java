bd4c46ae4010b7485a900a5852ee9121c5772e1f
AndreyZartty <andreysansab@gmail.com>
2018-10-01 02:14:20 -0600

    Redefinicion clase Jugador

--- Jugador.java
+++ Jugador.java
@@ -1,19 +1,32 @@
 package dots;
 
 public class Jugador {
-    private LinkedList figuras;
     private int puntaje;
     private int turno;
+    private boolean activo= false;
     
     public Jugador(int turno){
         this.turno=turno;
     }
-    
-
-    public void setFigura(Figura nueva){
-        figuras.insertFirst(nueva);
-
+    public boolean getEstado(){
+        return this.activo;
     }
-
+    public void setEstado(){
+        if (this.activo){
+            this.activo=false;
+        }
+        else{
+            this.activo=true;
+        }
+    }
+    public int getTurn(){
+        return this.turno;
+    }
+    public void setPuntos(int puntos){
+        this.puntaje+= puntos;
+    }
+    public int getPuntaje(){
+        return this.puntaje;
+    }
 
 }
